73da976b2b15b5c83b1462c2ec9123e3e04972a5
Nitish Gadangi <nitish.gadangi@cred.club>
2024-07-08 23:20:13 +0530

    Minor : Add builder and ViewController

--- MovieDetails/Details/DetailsBuilder.swift
+++ MovieDetails/Details/DetailsBuilder.swift
@@ -6,5 +6,22 @@
 //
 
 import Foundation
+import UIKit
+import NetworkCore
 
+protocol VIPERBaseBuilder {
+    static func build() -> UIViewController
+}
 
+struct MovieDetailsBuilder: VIPERBaseBuilder {
+    static func build() -> UIViewController {
+        let networkManager = NetworkManager<MoviesAPI>(env: .stage)
+        let repository = DetailsRepository(networkManager: networkManager) // mock repo can be injected if required
+        let interactor = DetailsInteractor(repository: repository)
+        let router = DetailsRouter()
+        let presenter = DetailsPresenter(interactor: interactor, router: router)
+        let viewController = DetailsViewController(presenter: presenter)
+        router.viewController = viewController
+        return viewController
+    }
+}
--- MovieDetails/Details/ViewController/DetailsViewController.swift
+++ MovieDetails/Details/ViewController/DetailsViewController.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import UIKit
+
+final class DetailsViewController: UIViewController, DetailsViewControllerProtocol {
+    private let presenter: DetailsPresenterProtocol
+
+    init(presenter: DetailsPresenterProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupBindings()
+        presenter.input.viewLoaded.onNext(())
+    }
+}
+
+extension DetailsViewController {
+    func popOrDismiss() {
+        // do any other heavy lifting if needed
+        dismiss(animated: true)
+    }
+}
+
+private extension DetailsViewController {
+    func setupUI() {
+        addSubViews()
+        setupConstraints()
+        setupViews()
+    }
+
+    func addSubViews() {}
+
+    func setupConstraints() {}
+
+    func setupViews() {}
+
+    func setupBindings() {}
+}
